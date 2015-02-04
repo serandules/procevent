0c28535331599f6d5908faec33a0dc24027ee29c
ruchiraw <ruchira.wageesha@gmail.com>
2015-02-04 22:54:01 +0530

    updating

--- index.js
+++ index.js
@@ -1,45 +1,74 @@
+var log = require('logger')('procevent');
 var util = require('util');
+var uuid = require('node-uuid');
 var events = require('events');
 
 var Process = function (process) {
     this.process = process;
     this.onces = {};
     this.ons = {};
-    var that = this;
+    this.queue = {};
+    var thiz = this;
     process.on('message', function (o) {
         if (!o.event) {
             return
         }
-        var cbs = that.ons[o.event];
-        if (cbs) {
-            cbs.forEach(function (cb) {
-                cb.apply(that, o.data);
+        var done;
+        if (o.event === 'emitted') {
+            if (log.debug) {
+                log.debug('emiton response %s, %s', o.id, thiz.process.pid);
+            }
+            done = thiz.queue[o.id];
+            done.apply([false].concat(o.data));
+            delete thiz.queue[o.id];
+            return;
+        }
+        var dones = thiz.ons[o.event];
+        var args = (o.event === 'emit') ? [o.event, o.id].concat(o.data) : o.data;
+        if (dones) {
+            dones.forEach(function (done) {
+                done.apply(thiz, args);
             });
         }
-        cbs = that.onces[o.event];
-        if (cbs) {
-            cbs.forEach(function (cb) {
-                cb.apply(that, o.data);
+        dones = thiz.onces[o.event];
+        if (dones) {
+            dones.forEach(function (done) {
+                done.apply(thiz, args);
             });
-            that.onces[o.event] = [];
+            thiz.onces[o.event] = [];
         }
     });
 };
 
-Process.prototype.on = function (event, cb) {
-    var cbs = this.ons[event] || (this.ons[event] = []);
-    cbs.push(cb);
+Process.prototype.on = function (event, done) {
+    var dones = this.ons[event] || (this.ons[event] = []);
+    dones.push(done);
 };
 
-Process.prototype.once = function (event, cb) {
-    var cbs = this.onces[event] || (this.onces[event] = []);
-    cbs.push(cb);
+Process.prototype.once = function (event, done) {
+    var dones = this.onces[event] || (this.onces[event] = []);
+    dones.push(done);
 };
 
 Process.prototype.emit = function (event) {
     this.process.send({
         event: event,
         data: Array.prototype.slice.call(arguments, 1)
+    });
+};
+
+Process.prototype.emiton = function (event) {
+    var id = uuid.v4();
+    var args = Array.prototype.slice.call(arguments, 1);
+    this.queue[id] = args.pop();
+    if (log.debug) {
+        log.debug('emiton request %s, %s, %s, %s', event, id, JSON.stringify(args), this.process.pid);
+    }
+    this.process.send({
+        event: 'emit',
+        type: event,
+        id: id,
+        data: args
     });
 };
 
